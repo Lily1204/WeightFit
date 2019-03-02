6a35544c86db1092f176dec512826d54fc0ed5f1
Liliana <molilis.pr95@live.com.mx>
2019-03-02 02:02:44 -0600

    .

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDm44xngeAbFE4K7YzgAT1eubsZ_mdpB5g",
+    authDomain: "weightfit-52010.firebaseapp.com",
+    databaseURL: "https://weightfit-52010.firebaseio.com",
+    projectId: "weightfit-52010",
+    storageBucket: "weightfit-52010.appspot.com",
+    messagingSenderId: "485208869993"
+  }
 };
